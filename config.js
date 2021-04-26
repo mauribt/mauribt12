8d47b774ed66e8c76b3488b5fc5ab3004b9c688b
mtmauri <lunaanalia301@gmail.com>
2021-04-25 23:10:14 -0300

    mauribt12

---
+++ config.js
@@ -0,0 +1,162 @@
+const ownerNumber = ["994407927354@s.whatsapp.net"] 
+// Tú número
+
+const OriginalOwner = '994407927354' 
+// Tú número x2
+
+const apikeyporn = 'fc8fe174f018b95cdae9272493f1a12f'
+// sua key do imgbb para ativar o antiporn
+
+const cr = '𝖒𝖆𝖚𝖗𝖎 𝖇𝖔𝖙\n' 
+// texto del menu
+
+const prefix = '!' 
+// prefixo
+
+const blockedmsg = 'estas bloqueado por el bot trolo de mierda'
+// mensagem quando alguem bloqueado solicita um comando
+
+const blockedcmdmsg = '𝙘𝙤𝙢𝙖𝙣𝙙𝙤 𝙗𝙡𝙤𝙦𝙪𝙚𝙖𝙙𝙤'
+// mensagem quando alguem solicita um comando bloqueado 
+
+// MENSAGENS DE BAN
+
+const banmsgtype = 'te vas a ir baneado'
+// mensagem de ban no anti tipos de mensagem
+
+const adminmsgtype = 'te salvaste pq sos admin hijo de puta'
+// mensagem de quando adm manda tipos de mensagens proibidas
+
+const banmsgporn = 'q pajero'
+// mensagem de ban no antiporn
+
+const adminmsgporn = 'admin pajero'
+// mensagem de quando adm manda porn com antiporn ativado
+
+const banmsglink = 'te vas a ir baneado'
+// mensagem de ban no antilink
+
+const adminmsglink = 'Te salvaste por ser admin 🙃'
+// mensagem de quando adm manda link
+
+const adminmsgpalavra = 'te salvaste pq sos admin hijo de puta'
+// mensagem de quando adm manda uma palavra proibida pelo bot
+
+const banmsgpalavra = 'te vas a ir baneado'
+// mensagem de ban quando manda uma palavra proibida pelo bot
+
+const qnttravahardroleta = 2
+// quantidade de travas que mandam no roleta russa hard
+// Obs: O valor é multiplicado por 12 ent tenha cuidado
+
+// MENSAGENS DE BOM DIA BOA TARDE E BOA NOITE
+
+const linkimgday = 'https://i.imgur.com/RR18JiI.jpg'
+const textmsgday = 'Wenos días guap@ 👉👈'
+// link e mensagem de bom dia
+
+const linkimgeve = 'https://i.ibb.co/bLXgcBW/lp-tarde-anime-by-hitsukinyan-db7fpqr-fullview.jpg'
+const textmsgeve = `Un caffe ☕`
+// link e mensagem de boa tarde
+
+const linkimgnig = 'https://i.ibb.co/VJCd34F/EKvo372-X0-AEd-Hz.jpg'
+const textmsgnig = 'Que tengas un buen dia '
+// link e mensagem de boa noite 
+
+// MENSAGENS DE ERRO
+
+const msgerr = 'error, intentalo de vuelta'
+// mensagem de erro
+
+// MENSAGENS DE AUTOREPLY
+
+const botlindo = 'gracias'
+const linkbotlindo = 'https://photos1.iorbix.com/00/00/00/00/02/72/43/64/C--6tZwaASQH-b.jpg'
+const botfeio = 'Calla cara de culo 😡'
+const cadebot = 'Me llamó onii-chan 👉👈?'
+const botfdp = 'Calla cagada'
+const botgostoso = 'Ojo 😳👉👈'
+const botfofo = 'Ojo 😳👉👈'
+const botbaianor = 'La tuya'
+const botcorno = 'Come verga'
+const botputa = 'Señora tu madre 😡'
+const botgay = 'cerra el ojete, gay tu papa pelotudo'
+const botviado = 'Calla pvto 😡'
+const numbotfeio = 'dama tu madre 😡'
+// KEYS
+
+const apiTobz = 'key'
+// pra pegar a key do Tobz va até o numero 
+//wa.me/447418349954 se registre no bot
+// e depois escreva #generateapikey que o 
+//bot do tobz vai gerar sua key
+
+// MENSAGENS DE EXCLUSIVIDADE
+let mess = {
+    wait: 'para',
+    success: 'listo',
+    error: {
+        stick: 'no se pudo convertir el sticker ',
+        Iv: 'no existe ese link gordo mogolico'
+    },
+    only: {
+        group: 'este comando solo se puede usar en grupos',
+        ownerG: 'solo el owner del grupo puede usar este comando',
+        ownerB: 'solo el owner del bot puede usar este comando',
+        admin: 'este comando solo puede ser usado por admins pelotudo',
+        Badmin: 'me olvide q iba aca x d'
+    }
+}
+
+// CONTATO DO CRIADOR
+
+const vcard = 'BEGIN:VCARD\n' 
++ 'VERSION:3.0\n' 
++ 'FN:Mi creador\n' 
++ 'ORG:𝖒𝖆𝖚𝖗𝖎;\n' 
++ 'TEL;type=CELL;type=VOICE;waid=994407927354:+994 40 792 73 54\n' 
++ 'END:VCARD'
+//vcard do propietário, mude os numeros de acordo com o formato que
+//ele corresponde
+
+
+// Nem ouse mexer aqui se n quiser que de erro
+exports.qnttravahardroleta = qnttravahardroleta
+exports.blockedcmdmsg = blockedcmdmsg
+exports.blockedmsg = blockedmsg
+exports.banmsgpalavra = banmsgpalavra
+exports.adminmsgpalavra = adminmsgpalavra
+exports.apikeyporn = apikeyporn
+exports.linkbotlindo = linkbotlindo
+exports.botlindo = botlindo
+exports.botfeio = botfeio
+exports.cadebot = cadebot
+exports.botfdp = botfdp
+exports.botgostoso = botgostoso
+exports.botfofo = botfofo
+exports.botbaianor = botbaianor
+exports.botcorno = botcorno
+exports.botputa = botputa
+exports.botgay = botgay
+exports.botviado = botviado
+exports.numbotfeio = numbotfeio
+exports.linkimgday = linkimgday
+exports.textmsgday = textmsgday
+exports.textmsgnig = textmsgnig
+exports.linkimgnig = linkimgnig
+exports.textmsgeve = textmsgeve
+exports.linkimgeve = linkimgeve
+exports.adminmsgtype = adminmsgtype
+exports.adminmsgporn = adminmsgporn
+exports.adminmsglink = adminmsglink
+exports.banmsglink = banmsglink
+exports.banmsgporn = banmsgporn
+exports.banmsgtype = banmsgtype
+exports.msgerr = msgerr
+exports.mess = mess
+exports.apiTobz = apiTobz
+exports.prefix = prefix
+exports.cr = cr
+exports.vcard = vcard
+exports.OriginalOwner = OriginalOwner
+exports.ownerNumber = ownerNumber
---
+++ welcomtext.js
@@ -0,0 +1,9 @@
+const welcome = (number, groupname) => {
+    return `ola @${number}`
+}
+exports.welcome = welcome
+
+const bye = (number) => {
+    return `porfin se fue @${number}, era insoportable`
+}
+exports.bye = bye
